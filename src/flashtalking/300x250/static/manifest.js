439eea546c78bc14e4e136d49327420a402d1d64
Luis Carlos Palomo <42524903+PalomoLuis@users.noreply.github.com>
2023-07-20 14:23:01 -0300

    dynamic flashtalking template

---
+++ src/flashtalking/300x250/static/manifest.js
@@ -0,0 +1,12 @@
+FT.manifest({
+  "filename": "index.html",
+  "width": 300,
+  "height": 250,
+  "clickTagCount": 1,
+  "hideBrowsers": ["ie8"],
+  "instantAds":[
+    {"name":"title", "type":"text", "default":"Welcome to this Banner!"},
+    {"name":"cta", "type":"text", "default":"CLICK HERE"},
+    {"name":"backgroundImage", "type":"image", "default":"bg.jpg"}
+  ]
+});
